33d6eb34289420d1386350dd8a2b84022c907726
Julian Cheal <julian.cheal@gmail.com>
2015-06-08 17:56:54 -0400

    updated demo

--- examples/simple.rb
+++ examples/simple.rb
@@ -2,16 +2,23 @@ require 'artoo'
 
 connection :mqtt, :adaptor => :mqtt, :port => '127.0.0.1:1883'
 device :mqtt_greet, :driver => :mqtt, :connection => :mqtt, :topic => "greeting"
+device :mqtt_time, :driver => :mqtt, :connection => :mqtt, :topic => "time"
 
 work do
   on mqtt_greet, :greeting => proc { |*value|
     puts "Message: #{value}"
   }
 
-  counter = 0
+  on mqtt_time, :time => proc { |*value|
+    puts "Message: #{value}"
+  }
 
   every(1.seconds) do
-    mqtt_greet.publish_message('greeting', "Hello #{counter+=1}")
+    mqtt_greet.publish_message('greeting', "Hello world!")
+  end
+
+  every(1.seconds) do
+    mqtt_time.publish_message('time', Time.now)
   end
 
 end
